f0aabf7a15f3c6ca7cdbdaae39eef6c889ea7f3f
premtechie <premtechie07@gmail.com>
2020-10-12 15:10:19 +0530

    Updates

---
+++ precache-manifest.6d7285d4c62ead36c070957eae0d05d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbbdd4b48ad69ff26f2803779c610e2",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "eee4e9950ba9c839a22d",
+    "url": "/shopping-app/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "6beabed5d7fbce1129a6",
+    "url": "/shopping-app/static/css/main.edf6a0f7.chunk.css"
+  },
+  {
+    "revision": "eee4e9950ba9c839a22d",
+    "url": "/shopping-app/static/js/2.9022dd29.chunk.js"
+  },
+  {
+    "revision": "ee24df130e0771fa560a257ad60b5b55",
+    "url": "/shopping-app/static/js/2.9022dd29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6beabed5d7fbce1129a6",
+    "url": "/shopping-app/static/js/main.4c87b150.chunk.js"
+  },
+  {
+    "revision": "421f46c2a92e7c10a353",
+    "url": "/shopping-app/static/js/runtime-main.6665a87d.js"
+  },
+  {
+    "revision": "0881a0402a361abbd3a325afe8abd4fc",
+    "url": "/shopping-app/static/media/image1.0881a040.png"
+  },
+  {
+    "revision": "6b0b15d981526771dced3f5c9640bcfa",
+    "url": "/shopping-app/static/media/image1.6b0b15d9.png"
+  },
+  {
+    "revision": "f74102e1ba753e43092d9a5cedd8bec6",
+    "url": "/shopping-app/static/media/image1.f74102e1.png"
+  },
+  {
+    "revision": "95aa6a885d199ba4507a0732dc2279ea",
+    "url": "/shopping-app/static/media/image2.95aa6a88.png"
+  },
+  {
+    "revision": "d4ba84c4131a2f6cd10778dd4aad0eab",
+    "url": "/shopping-app/static/media/image2.d4ba84c4.png"
+  },
+  {
+    "revision": "f1798bd55f00b120495cb6f2a3fd83a4",
+    "url": "/shopping-app/static/media/image2.f1798bd5.png"
+  },
+  {
+    "revision": "b3e5adc5e9dc77d3c065748af90f8597",
+    "url": "/shopping-app/static/media/image3.b3e5adc5.png"
+  },
+  {
+    "revision": "dfc6cf6cd7a05f41ba1c6a05b9e7ed36",
+    "url": "/shopping-app/static/media/image4.dfc6cf6c.png"
+  },
+  {
+    "revision": "9bb4eff61d12087648559078923e3508",
+    "url": "/shopping-app/static/media/image5.9bb4eff6.png"
+  },
+  {
+    "revision": "eb9b429a00cb8e989999fc399bc056d4",
+    "url": "/shopping-app/static/media/yourchoice.eb9b429a.png"
+  }
+]);
